238b82e980782b88a2e3858f41faad42d384815f
Nadezhda Brazhnikova <b.nadejda.v@mail.ru>
2020-04-17 19:04:08 +0300

    Завершает вёрстку личного проекта

--- source/js/popup.js
+++ source/js/popup.js
@@ -1,21 +1,31 @@
-var button = document.querySelector(".button__order");
-var popup = document.querySelector(".popup__wrapper-add");
+var button = document.querySelectorAll(".button__order");
+var popup = document.querySelector(".popup__order");
 var overlay = document.querySelector(".popup__overlay")
 
-  button.addEventListener("click", function (evt) {
+for (var i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", function (evt) {
+    evt.preventDefault(),
+      popup.classList.add("popup__order--show"),
+      overlay.classList.add("popup__overlay--show")
+  })
+};
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
     evt.preventDefault();
-    popup.classList.add("popup__wrapper-show");
-    overlay.classList.add("popup__overlay--show");
-  });
+    if (popup.classList.contains("popup__order--show")) {
+      popup.classList.remove("popup__order--show");
+      overlay.classList.remove("popup__overlay--show");
+    }
+  }
+});
 
-
-/*overlay.addEventListener("click", function (evt) {
-  evt.preventDefault();
-    popup.classList.remove("popup__wrapper-show"),
-    overlay.classList.remove("popup__overlay--show")
-  }),*/
-
-window.addEventListener("keydown", function (e) {
-  27 === e.keyCode && popup.classList.contains("popup__wrapper-show") && (popup.classList.remove("popup__wrapper-show"),
-    overlay.classList.remove("popup__overlay--show"))
+window.addEventListener("click", function (evt) {
+  if (evt.target === overlay) {
+    evt.preventDefault();
+    if (popup.classList.contains("popup__order--show")) {
+      overlay.classList.remove("popup__overlay--show");
+      popup.classList.remove("popup__order--show");
+    }
+  }
 });
